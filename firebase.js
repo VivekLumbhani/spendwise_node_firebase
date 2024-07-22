192f2e7b08b4567fc4150083f0388a347d8b30ec
VivekLumbhani <116283316+VivekLumbhani@users.noreply.github.com>
2024-07-22 16:20:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
 const admin = require('firebase-admin');
-const moment = require('moment');
 const serviceAccount = require('./config/spendwiseproject-firebase-adminsdk-mjlqr-4eefad7c51.json');
 
 admin.initializeApp({
@@ -8,39 +7,4 @@ admin.initializeApp({
 });
 
 const db = admin.database();
-
-const formatTimestamp = (timestamp) => {
-  return moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
-};
-
-const fetchAndFormatSMS = async () => {
-  try {
-    const smsRef = db.ref('sms');
-    const formattedSMSRef = db.ref('formatted_sms');
-    const snapshot = await smsRef.once('value');
-    const smsData = snapshot.val();
-
-    if (!smsData) {
-      console.log('No SMS data found.');
-      return;
-    }
-
-    for (const key in smsData) {
-      if (smsData.hasOwnProperty(key)) {
-        const sms = smsData[key];
-
-        const formattedTimestamp = formatTimestamp(sms.timestamp);
-        const formattedSMS = {
-          ...sms,
-          formattedTimestamp,
-        };
-        await formattedSMSRef.child(key).set(formattedSMS);
-      }
-    }
-    console.log('Formatted SMS data saved successfully.');
-  } catch (error) {
-    console.error('Error fetching and formatting SMS data:', error);
-  }
-};
-
-fetchAndFormatSMS();
+module.exports = db;
